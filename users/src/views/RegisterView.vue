61271f507243d826eafb931bea6e5c18579d88e0
lucasplr <lucasriffel55@gmail.com>
2022-09-08 21:58:48 -0300

    register form

---
+++ users/src/views/RegisterView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h2>Registro de usuário!</h2>
+        <hr>
+
+            <div class="columns is-centered">
+        <div class="column is-half">
+                <p>Nome</p>
+                <input type="text" name=""  placeholder="Nome do Usuário" class="input">
+                <p>Email</p>
+                <input type="email" name=""  placeholder="example@email.com" class="input">
+                <p>Senha</p>
+                <input type="password" name=""  placeholder="*******" class="input">
+                <button class="button">Cadastrar</button>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default{
+    setup() {
+        
+    },
+}
+
+</script>
+
+<style scoped>
+    #nameInput{
+        width: 500px;
+    }
+</style>
